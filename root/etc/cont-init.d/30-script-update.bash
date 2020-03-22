8bba7096e718d45b715adcab6aa76eac2127b0df
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2020-03-22 14:54:57 -0400

    Add files via upload

---
+++ root/etc/cont-init.d/30-script-update.bash
@@ -0,0 +1,9 @@
+#!/usr/bin/with-contenv bash
+
+# update from git
+if [[ "${UPDATE_SMA}" == "TRUE" ]]; then
+    git -C ${SMA_PATH} reset --hard HEAD && \
+    git -C ${SMA_PATH} pull origin master
+fi
+
+exit 0
