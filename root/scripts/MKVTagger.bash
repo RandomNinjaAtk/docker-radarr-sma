dd7510dab18b436452d2c383a252d2fa06f00878
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2020-07-18 18:28:55 -0400

    New script to tag mkv files with metadata

---
+++ root/scripts/MKVTagger.bash
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+apikey="$(grep "<ApiKey>" /config/config.xml | sed "s/\  <ApiKey>//;s/<\/ApiKey>//")"
+RadarrUrl="http://127.0.0.1:7878"
+scriptpath="/config/scripts"
+
+exec &> "$scriptpath/MKVTagger.log"
+
+radarrid="$radarr_movie_id"
+radarrmoviedata="$(curl -s --header "X-Api-Key:"${apikey} --request GET  "$RadarrUrl/api/movie/$radarrid")"
+radarrmoviecredit="$(curl -s --header "X-Api-Key:"${apikey} --request GET  "$RadarrUrl/api/v3/credit?movieId=$radarrid")"
+radarrmoviedirector="$(echo "${radarrmoviecredit}" | jq -r ".[] | select(.job==\"Director\") | .personName"  | head -n 1)"
+radarrmovietitle="$(echo "${radarrmoviedata}" | jq -r ".title")"
+radarrmovieyear="$(echo "${radarrmoviedata}" | jq -r ".year")"
+radarrmoviepath="$(echo "${radarrmoviedata}" | jq -r ".path")"
+radarrmoviegenre="$(echo "${radarrmoviedata}" | jq -r ".genres | .[]" | head -n 1)"
+radarrmoviefolder="$(basename "${radarrmoviepath}")"
+radarrmoviecertification="$(echo "${radarrmoviedata}" | jq -r ".certification")"
+radarrmovieoverview="$(echo "${radarrmoviedata}" | jq -r ".overview")"
+radarrmoviefilename="$(echo "${radarrmoviedata}" | jq -r ".movieFile.relativePath")"
+radarrmovieostudio="$(echo "${radarrmoviedata}" | jq -r ".studio")"
+radarrtrailerid="$(echo "${radarrmoviedata}" | jq -r ".youTubeTrailerId")"
+youtubeurl="https://www.youtube.com/watch?v=$radarrtrailerid"
+if [ ! -d "$radarrmoviepath" ]; then
+    echo "Processing :: $radarrmovietitle :: ERROR: Movie Path does not exist ($radarrmovietitle), Skipping..."
+    exit 0
+fi
+if [ ${radarrmoviefilename: -4} == ".mkv" ]; then
+	echo "Processing :: $radarrmovietitle :: $radarrmoviefilename"
+	mv "$radarrmoviepath/$radarrmoviefilename" "$radarrmoviepath/temp.mkv"
+	ffmpeg -y \
+		-i "$radarrmoviepath/temp.mkv" \
+		-c:v copy \
+		-c:a copy \
+		-c:s copy \
+		-metadata TITLE="${radarrmovietitle}" \
+		-metadata DATE_RELEASE="$radarrmovieyear" \
+		-metadata DATE="$radarrmovieyear" \
+		-metadata YEAR="$radarrmovieyear" \
+		-metadata GENRE="$radarrmoviegenre" \
+		-metadata COPYRIGHT="$radarrmovieostudio" \
+		-metadata COMMENT="$radarrmovieoverview" \
+		-metadata DIRECTOR="$radarrmoviedirector" \
+		-attach "/config/MediaCover/$radarrid/poster.jpg" -metadata:s:t mimetype=image/jpeg \
+		"$radarrmoviepath/$radarrmoviefilename" &> /dev/null
+	if 	[ -f "$radarrmoviepath/$radarrmoviefilename" ]; then
+		rm "$radarrmoviepath/temp.mkv"
+		echo "Processing :: $radarrmovietitle :: Updating File Statistics"
+		mkvpropedit "$radarrmoviepath/$radarrmoviefilename" --add-track-statistics-tags
+		echo "Processing :: $radarrmovietitle :: Complete!"
+	else
+		echo "Processing :: $radarrmovietitle :: Failed!"
+		mv "$radarrmoviepath/temp.mkv" "$radarrmoviepath/$radarrmoviefilename"
+	fi
+fi
+exit 0
