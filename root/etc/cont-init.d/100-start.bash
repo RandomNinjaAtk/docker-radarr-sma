6ea86bd949aa0f8a54418bc8cf1a985e155ff5cc
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2021-03-30 17:33:44 -0400

    Add files via upload

---
+++ root/etc/cont-init.d/100-start.bash
@@ -0,0 +1,13 @@
+#!/usr/bin/with-contenv bash
+echo "------------------------------------------------------------"
+echo "          _                                      "
+echo "         |_) _.._  _| _ ._ _ |\ |o._ o _. /\_|_| "
+echo "         | \(_|| |(_|(_)| | || \||| ||(_|/--\|_|<"
+echo "                                    _|           "
+echo "               Presenets: radarr-sma"
+echo "------------------------------------------------------------"
+echo "Donate: https://github.com/sponsors/RandomNinjaAtk"
+echo "Project: https://github.com/RandomNinjaAtk/docker-radarr-sma"
+echo "Support: https://discord.gg/JumQXDc"
+echo "------------------------------------------------------------"
+exit 0
